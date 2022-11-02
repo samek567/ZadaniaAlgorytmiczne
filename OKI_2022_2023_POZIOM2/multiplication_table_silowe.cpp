ac52f69fabfb9c28d3f85ce4c4a130bd47c8bad9
Samuel Maj <dalin.samuel@gmail.com>
2022-11-02 15:04:11 +0100

    OKI Poziom 6Challenge 6 - Drzewa Przedzialowe

---
+++ OKI_2022_2023_POZIOM2/multiplication_table_silowe.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n = 0;
+vector<long long> liczby;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n;
+
+    for (int i = 1; i <= n; ++i)
+    {
+        long long suma = 0;
+        for (int j = 0; j < n; ++j)
+        {
+            suma += i;
+            liczby.push_back(suma);
+        }
+    }
+
+    sort(liczby.begin(),liczby.end());
+
+    cout << liczby[n * n / 2] << "\n";
+
+    return 0;
+}
---
+++ OKI_2022_2023_POZIOM2/spacery_po_drzewie_binarnym.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int q = 0, wynik = 0;
+long long a = 0, b = 0;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> q;
+
+    for (int i = 0; i < q; ++i)
+    {
+        cin >> a >> b;
+        wynik = 0;
+
+        while (a != b)
+        {
+            if (a > b)
+            {
+                a /= 2;
+            }
+            else
+            {
+                b /= 2;
+            }
+            wynik++;
+        }
+
+        cout << wynik << "\n";
+    }
+    return 0;
+}
