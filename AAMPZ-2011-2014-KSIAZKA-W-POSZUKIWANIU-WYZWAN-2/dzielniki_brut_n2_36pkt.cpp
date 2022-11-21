f7fe221a81e576c66a67857ddd15c74ab469be0c
Samuel Maj <dalin.samuel@gmail.com>
2022-11-21 23:35:34 +0100

    Dzielniki 2014 brut n^2 36pkt 1 gwiazdka

---
+++ AAMPZ-2011-2014-KSIAZKA-W-POSZUKIWANIU-WYZWAN-2/dzielniki_brut_n2_36pkt.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n = 0, wczytana_liczba = 0;
+long long wyn = 0;
+vector<int> liczby;
+
+int main()
+{
+    // Dzielniki 2014 1 gwiazdka
+    // O(n^2)
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n;
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> wczytana_liczba;
+        liczby.push_back(wczytana_liczba);
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = i+1; j < n; ++j)
+        {
+            if (liczby[i] == liczby[j])
+            {
+                wyn += 2;
+            }
+            else if (liczby[i] > liczby[j] && liczby[i] % liczby[j] == 0)
+            {
+                wyn++;
+            }
+            else if (liczby[i] < liczby[j] && liczby[j] % liczby[i] == 0)
+            {
+                wyn++;
+            }
+        }
+    }
+
+    cout << wyn << '\n';
+
+    return 0;
+}
