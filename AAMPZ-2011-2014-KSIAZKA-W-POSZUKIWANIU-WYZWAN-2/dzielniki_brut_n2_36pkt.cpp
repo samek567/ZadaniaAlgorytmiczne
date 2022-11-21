ff4b5cf1c2aa9f3a1b44a2f2426cb38e3c8362ea
Samuel Maj <dalin.samuel@gmail.com>
2022-11-21 23:33:13 +0100

    Dzielniki 2014 szybkie 62pkt O(n * sqrt(max_a_i)), 1 gwiazdka

--- AAMPZ-2011-2014-KSIAZKA-W-POSZUKIWANIU-WYZWAN-2/dzielniki_brut_n2_36pkt.cpp
+++ AAMPZ-2011-2014-KSIAZKA-W-POSZUKIWANIU-WYZWAN-2/dzielniki_brut_n2_36pkt.cpp
@@ -3,13 +3,15 @@
 
 using namespace std;
 
-int n = 0, wczytana_liczba = 0;
+int n = 0, wczytana_liczba = 0, max_a_i = 0;
 long long wyn = 0;
 vector<int> liczby;
+vector<int> stat;
 
 int main()
 {
     // Dzielniki 2014 1 gwiazdka
+    // O(n * sqrt(max_a_i))
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
@@ -19,26 +21,29 @@ int main()
     for (int i = 0; i < n; ++i)
     {
         cin >> wczytana_liczba;
+        max_a_i = max(max_a_i,wczytana_liczba);
         liczby.push_back(wczytana_liczba);
+    }
+    stat.assign(max_a_i+1,0);
+    for (int i = 0; i < n; ++i)
+    {
+        stat[liczby[i]]++;
     }
 
     for (int i = 0; i < n; ++i)
     {
-        for (int j = i+1; j < n; ++j)
+        for (int j = 1; j * j <= liczby[i]; ++j)
         {
-            if (liczby[i] == liczby[j])
+            if (liczby[i] % j == 0)
             {
-                wyn += 2;
-            }
-            else if (liczby[i] > liczby[j] && liczby[i] % liczby[j] == 0)
-            {
-                wyn++;
-            }
-            else if (liczby[i] < liczby[j] && liczby[j] % liczby[i] == 0)
-            {
-                wyn++;
+                wyn += stat[j];
+                if (j != liczby[i]/j)
+                {
+                    wyn += stat[liczby[i]/j];
+                }
             }
         }
+        wyn--;
     }
 
     cout << wyn << '\n';
