f8d84d4b41155ecd154ca36c3e9ee9f393e3fdc4
Samuel Maj <dalin.samuel@gmail.com>
2022-11-23 21:10:29 +0100

    Wina Dywizja B

---
+++ PotyczkiAlgorytmiczne/wina.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int n = 0, k = 0, wczytana_liczba = 0, min_wyn = 1e8;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n >> k;
+
+    for (int i = 1; i <= n; ++i)
+    {
+        for (int j = 0; j < i; ++j)
+        {
+            cin >> wczytana_liczba;
+            if ((i-j) * (j+1) <= k)
+            {
+                min_wyn = min(min_wyn,wczytana_liczba);
+            }
+        }
+    }
+
+    cout << min_wyn << '\n';
+
+    return 0;
+}
