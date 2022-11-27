94d3f09232fa3b1c9d37429ee75d18a3902f8e87
Samuel Maj <dalin.samuel@gmail.com>
2022-11-28 00:57:26 +0100

    Challange 8 teleportacja potegi 2

---
+++ OKI_2022_2023_ZAWANSOWANE/challange8_teleportacja.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+long long n = 0, sum = 0, wyn = 0, iloczyn = 1, ile_brakuje = 0;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n;
+
+    while(sum+iloczyn <= n)
+    {
+        sum += iloczyn;
+        iloczyn *= 2;
+        wyn++;
+    }
+    iloczyn /= 2;
+    ile_brakuje = n-sum;
+
+    while (ile_brakuje > 0)
+    {
+        while(ile_brakuje >= iloczyn)
+        {
+            ile_brakuje -= iloczyn;
+            wyn++;
+        }
+        iloczyn /= 2;
+    }
+
+    cout << wyn << '\n';
+
+    return 0;
+}
