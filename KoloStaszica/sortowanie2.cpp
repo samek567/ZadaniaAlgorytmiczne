0ca58ea54bdedb9610772198da9026ea1f9ffe2c
Samuel Maj <dalin.samuel@gmail.com>
2022-12-26 01:30:15 +0100

    Hashe, Paradoks dnia urodzin 100pkt

---
+++ KoloStaszica/sortowanie2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n = 0, wczytana_liczba = 0;
+vector<int> liczby;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> wczytana_liczba;
+        liczby.push_back(wczytana_liczba);
+    }
+    sort(liczby.begin(),liczby.end());
+    for (int i = 0; i < n; ++i)
+        printf("%d ",liczby[i]);
+    return 0;
+}
