d4412af813ca7dca4815cee94d5bb6ac8566f3b5
Samuel Maj <dalin.samuel@gmail.com>
2023-01-29 21:04:17 +0100

    Union Findy OKI Zawansowane Challange 11

---
+++ OKI_2022_2023_ZAWANSOWANE/challange11_dynamiczna_spojnosc_union_find100pkt.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n = 0, m = 0, wczytana_liczba = 0, a_i = 0, b_i = 0, c_i = 0;
+vector<int> rep(5e5+5);
+
+int findd(int s)
+{
+    int wyn = 0, v = s;
+    while (true)
+    {
+        if (v == rep[v])
+        {
+            wyn = v;
+            break;
+        }
+        v = rep[v];
+    }
+    v = s;
+    while (true)
+    {
+        if (v == rep[v])
+            break;
+        int syn = rep[v];
+        rep[v] = wyn;
+        v = rep[v];
+    }
+    return wyn;
+}
+
+void unionn(int x, int y)
+{
+    rep[findd(x)] = findd(y);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n >> m;
+    for (int i = 1; i <= n; ++i)
+        rep[i] = i;
+    while(m--)
+    {
+        cin >> a_i >> b_i >> c_i;
+        if (a_i == 1)
+            unionn(b_i,c_i);
+        else
+        {
+            if (findd(b_i) == findd(c_i))
+                printf("TAK\n");
+            else
+                printf("NIE\n");
+        }
+    }
+    return 0;
+}
---
+++ OKI_2022_2023_ZAWANSOWANE/challange11_tunele100pkt_union_find.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n = 0, m = 0, k = 0, a_i = 0, b_i = 0, ile_spojnych = 0, wyn = 1e7;
+vector<int> rep(1e5+5,0);
+vector<pair<int,int>> krawedzie(2e5+5,{1,1});
+
+int findd(int s)
+{
+    int wyn = 0, v = s;
+    while (true)
+    {
+        if (v == rep[v])
+        {
+            wyn = v;
+            break;
+        }
+        v = rep[v];
+    }
+    v = s;
+    while (true)
+    {
+        if (v == rep[v])
+            break;
+        int syn = rep[v];
+        rep[v] = wyn;
+        v = syn;
+    }
+    return wyn;
+}
+
+void unionn(int x, int y)
+{
+    rep[findd(x)] = findd(y);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n >> m >> k;
+    for (int i = 1; i <= n; ++i)
+        rep[i] = i;
+    ile_spojnych = n;
+    for (int i = 0; i < m; ++i)
+    {
+        cin >> a_i >> b_i;
+        krawedzie[i] = {a_i, b_i};
+    }
+
+    for (int i = m-1; i >= 0; --i)
+    {
+        a_i = krawedzie[i].first, b_i = krawedzie[i].second;
+        if (findd(a_i) != findd(b_i))
+            ile_spojnych--;
+        if (ile_spojnych == k)
+            wyn = min(wyn,i+1);
+        unionn(a_i,b_i);
+    }
+
+    printf("%d\n",wyn);
+
+    return 0;
+}
