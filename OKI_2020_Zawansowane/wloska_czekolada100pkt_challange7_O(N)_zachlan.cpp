9e3d35d3be5fca4bf173df3d088096d9e38c1c04
Samuel Maj <dalin.samuel@gmail.com>
2023-02-10 00:26:34 +0100

    Aktualizacja zadanka Wloska Czekolada dodanie linka do forum

--- OKI_2020_Zawansowane/wloska_czekolada100pkt_challange7_O(N)_zachlan.cpp
+++ OKI_2020_Zawansowane/wloska_czekolada100pkt_challange7_O(N)_zachlan.cpp
@@ -10,6 +10,7 @@ vector<int> ile_mamy(1e6+1,0);
 
 int main()
 {
+    // https://forum.pasja-informatyki.pl/579688/c-vector-assign-zlozonnosc-czasowa-zadanie-wloska-czekolada-ix-oig
     // O(N), wykonamy 2N operacji. Bo petla j lacznie wykona sie N razy.
     // Wchodzi zachlan. Zawsze oplaca nam sie wziac najmniejszy fragment, ktory jest unitakowy, bo bez sensu kompletnie robic go jeszcze dluzszym.
     ios_base::sync_with_stdio(0);
