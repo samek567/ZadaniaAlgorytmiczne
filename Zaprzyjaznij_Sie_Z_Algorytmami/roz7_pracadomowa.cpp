dd739b85f3efa19b9e725d4cee73bdebb11a7d18
Samuel Maj <dalin.samuel@gmail.com>
2023-02-28 22:50:49 +0100

    Programowanie dynamiczne, spojny podciag o maksymalnej sumie

--- Zaprzyjaznij_Sie_Z_Algorytmami/roz7_pracadomowa.cpp
+++ Zaprzyjaznij_Sie_Z_Algorytmami/roz7_pracadomowa.cpp
@@ -2,40 +2,42 @@
 #include <vector>
 
 using namespace std;
+typedef long long ll;
 
-int n = 0;
-long long wczytana_liczba = 0, max_wyn = 0;
-vector<long long> liczby;
-vector<long long> prefix_max;
-vector<long long> sufix_max;
+int n = 0, wczytana_liczba = 0;
+vector<int> liczby;
+vector<ll> dp_prefix;
+vector<ll> dp_sufix;
+ll wyn = 0;
 
 int main()
 {
+    // O(N), programowanie dynamiczne. Szukamy od lewej i prawej spojnego podciagu o maksymalnej sumie dynamicznie. W odtwarzaniu,
+    // dla kazdego punktu zakladamy, ze jest on srodkowym i bierzemy max wynik.
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
 
     cin >> n;
-    prefix_max.assign(n,0);
-    sufix_max.assign(n,0);
     for (int i = 0; i < n; ++i)
     {
         cin >> wczytana_liczba;
         liczby.push_back(wczytana_liczba);
     }
 
-    prefix_max[1] = liczby[1];
-    for (int i = 2; i < n; ++i)
-        prefix_max[i] = max(prefix_max[i-1] + liczby[i], liczby[i]);
+    dp_prefix.assign(n,0);
+    dp_sufix.assign(n,0);
 
-    sufix_max[n-2] = liczby[n-2];
-    for (int i = n-3; i >= 0; --i)
-        sufix_max[i] = max(sufix_max[i+1] + liczby[i], liczby[i]);
+    for (int i = 1; i < n; ++i)
+        dp_prefix[i] = max((ll)liczby[i], max(liczby[i] + dp_prefix[i-1], (ll)0));
+
+    for (int i = n-2; i >= 0; --i)
+        dp_sufix[i] = max((ll)liczby[i], max(liczby[i] + dp_sufix[i+1], (ll)0));
 
     for (int i = 1; i < n-1; ++i)
-        max_wyn = max(max_wyn,prefix_max[i-1] + sufix_max[i+1]);
+        wyn = max(wyn, dp_prefix[i-1] + dp_sufix[i+1]);
 
-    printf("%lld",max_wyn);
+    cout << wyn << '\n';
 
     return 0;
 }
