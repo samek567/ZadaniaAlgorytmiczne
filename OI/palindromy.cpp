e717905bdeef1151ab8626be4306e9c4cb327ed1
Samuel Maj <dalin.samuel@gmail.com>
2023-03-17 12:32:43 +0100

    Palindrom 2 OI, dynamik z hashami 100pkt O(N^2)

--- OI/palindromy.cpp
+++ OI/palindromy.cpp
@@ -1,176 +1,130 @@
 #include <iostream>
 #include <vector>
-#include <queue>
-#include <set>
 
 using namespace std;
+typedef long long ll;
 
-struct Element
+int odtw = 0, mod = 1e9+7, p = 43, hash_przod = 0, hash_tyl = 0, hash_akt = 0;
+string ciag, spr;
+vector<int> dp_min;
+vector<int> dp_max;
+vector<int> idx_poprzedniej_min;
+vector<int> idx_poprzedniej_max;
+vector<int> POW;
+vector<int> hashe_prefiksowe;
+vector<int> hashe_sufixowe;
+vector<string> wyn_min;
+vector<string> wyn_max;
+
+inline bool czy_palindrom(int p, int k)
 {
-    int ostatni_idx;
-    vector<string> czesci;
-
-    bool operator < (const Element&element) const
-    {
-        return ostatni_idx < element.ostatni_idx;
-    }
-    bool operator == (const Element&element) const
-    {
-        return ostatni_idx == element.ostatni_idx;
-    }
-};
-
-bool czy_palindrom (string &ciag)
-{
-    int p = 0;
-    int k = ciag.size()-1;
-
-    while (k >= p)
-    {
-        if (ciag[p] != ciag[k])
-        {
-            return false;
-        }
-        p++;
-        k--;
-    }
-    return true;
+    if (p == 0)
+        hash_przod = ((ll)hashe_prefiksowe[k] + (ll)mod) % (ll)mod;
+    else
+        hash_przod = ((ll)hashe_prefiksowe[k] - (ll)hashe_prefiksowe[p-1] + (ll)mod) % (ll)mod;
+    if (k == ciag.size()-1)
+        hash_tyl = ((ll)hashe_sufixowe[p] + (ll)mod) % (ll)mod;
+    else
+        hash_tyl = ((ll)hashe_sufixowe[p] - (ll)hashe_sufixowe[k+1] + (ll)mod) % (ll)mod;
+    int p_poz = p, k_poz = ciag.size() - k - 1;
+    int roznica = abs(p_poz - k_poz);
+    if (p_poz > k_poz)
+        hash_tyl = ((ll)hash_tyl * (ll)POW[roznica] + (ll)mod) % (ll)mod;
+    else
+        hash_przod = ((ll)hash_przod * (ll)POW[roznica] + (ll)mod) % (ll)mod;
+    if (hash_przod == hash_tyl)
+        return true;
+    return false;
 }
 
 int main()
 {
+    // Programowanie dynamiczne, O(N^2). Sprawdzamy czy ciag jest palindormem w O(1), za pomca hashy.
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
 
-    string ciag;
-
     cin >> ciag;
-    vector<pair<string,int>> palindromy[ciag.size()];
-    queue<Element>Q;
-    vector<string> max_wynik;
-    vector<string> min_wynik;
+
+    POW.push_back(1);
+    for (int i = 0; i < ciag.size()+5; ++i)
+        POW.push_back(((ll)POW[i] * (ll)p) % (ll)mod);
+
+    hashe_prefiksowe.push_back((ll)ciag[0]);
+    for (int i = 1; i < ciag.size(); ++i)
+        hashe_prefiksowe.push_back(((ll)hashe_prefiksowe[i-1] + (ll)ciag[i] * (ll)POW[i] + (ll)mod) % (ll)mod);
+
+    hashe_sufixowe.assign(ciag.size(),0);
+    hashe_sufixowe[ciag.size()-1] = (ll)ciag[ciag.size()-1];
+    for (int i = ciag.size()-2; i >= 0; --i)
+        hashe_sufixowe[i] = ((ll)hashe_sufixowe[i+1] + (ll)ciag[i] * (ll)POW[ciag.size() - i - 1] + (ll)mod) % (ll)mod;
+
+    dp_min.assign(ciag.size()+1,-1);
+    dp_max.assign(ciag.size()+1,-1);
+    idx_poprzedniej_min.assign(ciag.size()+1,-1);
+    idx_poprzedniej_max.assign(ciag.size()+1,-1);
+
+    dp_min[0] = 0;
+    dp_max[0] = 0;
 
     for (int i = 0; i < ciag.size(); ++i)
     {
-        if (i % 2 != 0)
-        {
-            continue;
-        }
-        string prefix;
+        hash_akt = 0;
         for (int j = i; j < ciag.size(); ++j)
         {
-            prefix += ciag[j];
-            if (prefix.size() % 2 == 0)
+            hash_akt = ((ll)hash_akt + (ll)ciag[j] * POW[j]) % mod;
+            if ((j-i) % 2 == 1 and czy_palindrom(i,j) == true)
             {
-                if (czy_palindrom(prefix) == true)
+                if (dp_min[j+1] == -1 or dp_min[i] + 1 < dp_min[j+1])
                 {
-                    palindromy[i].push_back({prefix,j});
+                    dp_min[j+1] = dp_min[i] + 1;
+                    idx_poprzedniej_min[j+1] = i;
+                }
+                if (dp_max[j+1] == -1 or dp_max[i] + 1 > dp_max[j+1])
+                {
+                    dp_max[j+1] = dp_max[i] + 1;
+                    idx_poprzedniej_max[j+1] = i;
                 }
             }
         }
     }
 
-    // MIN
-    Q.push({-1,{}});
-    bool czy_bylismy[ciag.size()] {false};
-    if (ciag.size()== 1)
+    if (idx_poprzedniej_min[ciag.size()] == -1)
     {
-        cout << "NIE";
+        cout << "NIE" << '\n';
         return 0;
     }
-    while (!Q.empty())
+
+    odtw = ciag.size();
+    while(true)
     {
-        if (Q.front().ostatni_idx == ciag.size()-1)
-        {
-            int opcja = Q.front().czesci.size();
-            if (opcja < min_wynik.size() || min_wynik.size() == 0)
-            {
-                min_wynik = Q.front().czesci;
-            }
+        if (idx_poprzedniej_min[odtw] == -1)
             break;
-        }
-        else
-        {
-            for (int i = 0; i < palindromy[Q.front().ostatni_idx+1].size(); ++i)
-            {
-                if (czy_bylismy[palindromy[Q.front().ostatni_idx+1][i].second] == false)
-                {
-                    czy_bylismy[palindromy[Q.front().ostatni_idx+1][i].second] = true;
-                    Q.push({palindromy[Q.front().ostatni_idx+1][i].second,Q.front().czesci});
-                    Q.back().czesci.push_back(palindromy[Q.front().ostatni_idx+1][i].first);
-                }
-            }
-        }
-        Q.pop();
+        spr.clear();
+        for (int i = idx_poprzedniej_min[odtw]; i < odtw; ++i)
+            spr.push_back(ciag[i]);
+        wyn_min.push_back(spr);
+        odtw = idx_poprzedniej_min[odtw];
     }
 
-
-    // MAX
-    set<Element> elementy {{-1,{}}};
-    while (!elementy.empty())
+    odtw = ciag.size();
+    while(true)
     {
-        Element sprawdzany;
-        for (auto c : elementy)
-        {
-            sprawdzany = c;
+        if (idx_poprzedniej_max[odtw] == -1)
             break;
-        }
-        if (sprawdzany.ostatni_idx == ciag.size()-1)
-        {
-            int opcja = sprawdzany.czesci.size();
-            if (opcja > max_wynik.size() || max_wynik.size() == 0)
-            {
-                max_wynik = sprawdzany.czesci;
-            }
-        }
-        else
-        {
-            for (int i = 0; i < palindromy[sprawdzany.ostatni_idx+1].size(); ++i)
-            {
-                int szukany = palindromy[sprawdzany.ostatni_idx+1][i].second;
-                auto it = elementy.find({szukany});
-                if (it != elementy.end()) // Znalezlismy
-                {
-
-                    if (sprawdzany.czesci.size()+1 > it->czesci.size())
-                    {
-                        elementy.erase(it);
-                        vector<string> Vect;
-                        Vect = sprawdzany.czesci;
-                        Vect.push_back(palindromy[sprawdzany.ostatni_idx+1][i].first);
-                        elementy.insert({palindromy[sprawdzany.ostatni_idx+1][i].second,Vect});
-                    }
-
-                }
-                else
-                {
-                    vector<string> Vect;
-                    Vect = sprawdzany.czesci;
-                    Vect.push_back(palindromy[sprawdzany.ostatni_idx+1][i].first);
-                    elementy.insert({palindromy[sprawdzany.ostatni_idx+1][i].second,Vect});
-                }
-            }
-        }
-        elementy.erase({sprawdzany.ostatni_idx});
+        spr.clear();
+        for (int i = idx_poprzedniej_max[odtw]; i < odtw; ++i)
+            spr.push_back(ciag[i]);
+        wyn_max.push_back(spr);
+        odtw = idx_poprzedniej_max[odtw];
     }
 
-    if (max_wynik.size() == 0 || min_wynik.size() == 0)
-    {
-        cout << "NIE";
-    }
-    else
-    {
-        for (int i = 0; i < min_wynik.size(); ++i)
-        {
-            cout << min_wynik[i] << " ";
-        }
-        cout << endl;
-        for (int i = 0; i < max_wynik.size(); ++i)
-        {
-            cout << max_wynik[i] << " ";
-        }
-    }
+    for (int i = wyn_min.size()-1; i >= 0; --i)
+        cout << wyn_min[i] << ' ';
+    cout << '\n';
+    for (int i = wyn_max.size()-1; i >= 0; --i)
+        cout << wyn_max[i] << ' ';
+    cout << '\n';
 
     return 0;
 }
