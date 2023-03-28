dc7c0726dc6135cd03c2e30d908363589ecae769
Samuel Maj <dalin.samuel@gmail.com>
2023-03-28 15:01:27 +0200

    Skrzyzowanie 100pkt

---
+++ OIG/skrzyzowanie100pkt_obrot_wspolrzednych_mapa.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+struct Auto
+{
+    int ktory_pas;
+    int czas;
+};
+
+int n = 0, m = 0, p = 0, t = 0, ile_x = 0, ile_y = 0, wyn = 0;
+vector<Auto> poziomkowa;
+vector<Auto> truskawkowa;
+unordered_map<int,bool> stat;
+
+int main()
+{
+    // Podobny trik z obroceniem wspolrzednych i y-x wystapil w zadaniu binarne loty z Letniego Obozu po 15 OIJ, przed EJOI 2021,
+    // sprawdzarka i tresc zadania binarne loty jest na sio2.
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n >> m >> p >> t;
+
+    poziomkowa.assign(p,{0,0});
+    for (int i = 0; i < p; ++i)
+        cin >> poziomkowa[i].ktory_pas >> poziomkowa[i].czas;
+
+    truskawkowa.assign(t,{0,0});
+    for (int i = 0; i < t; ++i)
+        cin >> truskawkowa[i].ktory_pas >> truskawkowa[i].czas;
+
+    for (int i = 0; i < p; ++i)
+        ile_x = max(ile_x, poziomkowa[i].czas);
+
+    for (int i = 0; i < t; ++i)
+        ile_y = max(ile_y, truskawkowa[i].czas);
+
+    for (int i = 0; i < p; ++i)
+        stat[(poziomkowa[i].ktory_pas + ile_y) - (m + poziomkowa[i].czas)] = true;
+
+    for (int i = 0; i < t; ++i)
+        if (auto it = stat.find((ile_y - truskawkowa[i].czas + 1) - (m - truskawkowa[i].ktory_pas + 1)) == stat.end())
+            wyn++;
+
+    cout << wyn << '\n';
+
+    return 0;
+}
