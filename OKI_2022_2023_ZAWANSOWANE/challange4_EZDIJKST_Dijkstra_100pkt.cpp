01e715ba504cc710cb5cab99fc9425e7b63c8b5f
Samuel Maj <dalin.samuel@gmail.com>
2023-04-26 01:59:28 +0200

    OKI 2022/2023

--- OKI_2022_2023_ZAWANSOWANE/challange4_EZDIJKST_Dijkstra_100pkt.cpp
+++ OKI_2022_2023_ZAWANSOWANE/challange4_EZDIJKST_Dijkstra_100pkt.cpp
@@ -5,9 +5,8 @@ using namespace std;
 int main()
 {
     cout << "Zrobione." << '\n';
-/*
-ID rozwiazania: 30473265
-https://github.com/samek567/ZadaniaAlgorytmiczne/blob/main/OKI_2022_2023_ZAWANSOWANE/EDIJKSTR.cpp
-*/
+    /*
+    ID rozwiazania: 30473265
+    */
     return 0;
 }
